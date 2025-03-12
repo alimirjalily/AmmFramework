b624806b477a563234895464f507e523ab817a9d
alimirjalily <49408059+alimirjalily@users.noreply.github.com>
2025-03-13 01:32:38 +0330

    Add Logger Abstractions

---
+++ Extensions/AmmFramework.Extensions.Abstractions/AmmFramework.Extensions.Logger.Abstractions/FrameworkEventId.cs
@@ -0,0 +1,12 @@
+﻿namespace AmmFramework.Extensions.Logger.Abstractions;
+
+public class FrameworkEventId
+{
+    public const int PerformanceMeasurement = 1001;
+
+    public const int DomainValidationException = 1010;
+
+    public const int CommandValidation = 1011;
+    public const int QueryValidation = 1012;
+    public const int EventValidation = 1013;
+}
