39ef5bf3193fdc912e2053dfa50484c684718a90
alimirjalily <49408059+alimirjalily@users.noreply.github.com>
2025-03-12 15:47:03 +0330

    Add Caching Abstractions

---
+++ Extensions/AmmFramework.Extensions.Abstractions/AmmFramework.Extensions.Caching.Abstractions/FakeCacheAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace AmmFramework.Extensions.Caching.Abstractions;
+
+public class FakeCacheAdapter : ICacheAdapter
+{
+    public void Add<TInput>(string key, TInput obj, DateTime? absoluteExpiration, TimeSpan? slidingExpiration) { }
+
+    public TOutput Get<TOutput>(string key) => default;
+
+    public void RemoveCache(string key) { }
+}
