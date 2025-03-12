54d27e4989ff2302e6d1278845230d1aa18d94bb
alimirjalily <49408059+alimirjalily@users.noreply.github.com>
2025-03-13 01:54:56 +0330

    Add MessageBus Abstractions

---
+++ Extensions/AmmFramework.Extensions.Abstractions/AmmFramework.Extensions.MessageBus.Abstractions/IReceiveMessageBus.cs
@@ -0,0 +1,7 @@
+﻿namespace AmmFramework.Extensions.MessageBus.Abstractions;
+
+public interface IReceiveMessageBus
+{
+    void Subscribe(string serviceId, string eventName);
+    void Receive(string commandName);
+}
