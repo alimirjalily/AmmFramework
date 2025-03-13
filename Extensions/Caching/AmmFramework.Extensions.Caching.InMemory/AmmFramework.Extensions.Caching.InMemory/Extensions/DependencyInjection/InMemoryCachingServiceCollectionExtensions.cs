296b39abc7c1e88f14530874938c1b0c3273ff3b
alimirjalily <49408059+alimirjalily@users.noreply.github.com>
2025-03-13 15:08:57 +0330

    Add InMemory Implementation

---
+++ Extensions/Caching/AmmFramework.Extensions.Caching.InMemory/AmmFramework.Extensions.Caching.InMemory/Extensions/DependencyInjection/InMemoryCachingServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using AmmFramework.Extensions.Caching.InMemory.Services;
+using AmmFramework.Extensions.Caching.Abstractions;
+using AmmFramework.Extensions.Serializers.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AmmFramework.Extensions.DependencyInjection;
+
+public static class InMemoryCachingServiceCollectionExtensions
+{
+    public static IServiceCollection AddFrameworkInMemoryCaching(this IServiceCollection services)
+        => services.AddMemoryCache().AddTransient<ICacheAdapter, InMemoryCacheAdapter>();
+}
