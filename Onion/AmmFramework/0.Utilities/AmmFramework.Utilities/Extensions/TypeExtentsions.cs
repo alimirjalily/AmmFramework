808b4d3cf2a09b146306ca38a4f8a653fb2f187a
alimirjalily <49408059+alimirjalily@users.noreply.github.com>
2025-03-18 13:28:20 +0330

    Add Utilities

---
+++ Onion/AmmFramework/0.Utilities/AmmFramework.Utilities/Extensions/TypeExtentsions.cs
@@ -0,0 +1,20 @@
+﻿namespace AmmFramework.Utilities.Extensions
+{
+    public static class TypeExtensions
+    {
+
+        public static bool IsSubclassOfRawGeneric(this Type toCheck, Type generic)
+        {
+            while (toCheck != null && toCheck != typeof(object))
+            {
+                var cur = toCheck.IsGenericType ? toCheck.GetGenericTypeDefinition() : toCheck;
+                if (generic == cur)
+                {
+                    return true;
+                }
+                toCheck = toCheck.BaseType;
+            }
+            return false;
+        }
+    }
+}
---
+++ Onion/AmmFramework/0.Utilities/AmmFramework.Utilities/FrameworkServices.cs
@@ -0,0 +1,33 @@
+﻿using AmmFramework.Extensions.Caching.Abstractions;
+using AmmFramework.Extensions.ObjectMappers.Abstractions;
+using AmmFramework.Extensions.Serializers.Abstractions;
+using AmmFramework.Extensions.Translations.Abstractions;
+using AmmFramework.Extensions.UsersManagement.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace AmmFramework.Utilities;
+
+public class FrameworkServices
+{
+    public readonly ITranslator Translator;
+    public readonly ICacheAdapter CacheAdapter;
+    public readonly IMapperAdapter MapperFacade;
+    public readonly ILoggerFactory LoggerFactory;
+    public readonly IJsonSerializer Serializer;
+    public readonly IUserInfoService UserInfoService;
+
+    public FrameworkServices(ITranslator translator,
+        ILoggerFactory loggerFactory,
+        IJsonSerializer serializer,
+        IUserInfoService userInfoService,
+        ICacheAdapter cacheAdapter,
+        IMapperAdapter mapperFacade)
+    {
+        Translator = translator;
+        LoggerFactory = loggerFactory;
+        Serializer = serializer;
+        UserInfoService = userInfoService;
+        CacheAdapter = cacheAdapter;
+        MapperFacade = mapperFacade;
+    }
+}
